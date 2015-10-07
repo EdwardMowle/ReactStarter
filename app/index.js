7289a0463088bd68c3c49dc1b0dbbe648f7a3474
EdwardMowleCTM <edward.mowle@comparethemarket.com>
2015-10-07 14:20:24 +0100

    Simple hello world React app using webpack.

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Greeting from './components/greeting';
+
+React.render(
+    <Greeting name="World" />,
+    document.body
+);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loaders: ['babel-loader']
+		}, {
+			test: /\.html$/,
+			loaders: ['file?name=[name].[ext]']
+		}]
+	},
+	entry: {
+		javascript: __dirname + '/app/index.js',
+		html: __dirname + '/app/index.html'
+	},
+	output: {
+		path: __dirname + '/build',
+		filename: 'index.js'
+	}
+}
